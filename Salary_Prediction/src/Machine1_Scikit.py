b8bfcb35812df1eaaa39058cfe6560099b986001
Christy Chovalloor <christy.chovalloor@gmail.com>
2025-05-03 18:18:12 +0100

    Added scikit learn version

---
+++ Salary_Prediction/src/Machine1_Scikit.py
@@ -0,0 +1,14 @@
+from sklearn import datasets
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error,r2_score
+
+data = np.array(np.genfromtxt("Salary_Prediction/Data/Salary_Data.csv",delimiter=',',skip_header=1))
+X=data[:,0].reshape(-1,1)
+Y=data[:,1]
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.2)
+model = linear_model.LinearRegression()
+model.fit(X_train,Y_train)
+Y_pred = model.predict(X_test)
+print('R2 score: %.2f' % r2_score (Y_test,Y_pred))
