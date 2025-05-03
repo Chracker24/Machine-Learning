abdec5612758bbfdc9f1af8f74694f0f2d36c19e
Christy Chovalloor <christy.chovalloor@gmail.com>
2025-05-03 18:48:28 +0100

    Added Plotting

--- Salary_Prediction/src/Machine1_Scikit.py
+++ Salary_Prediction/src/Machine1_Scikit.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error,r2_score
+import matplotlib.pyplot as plt
 
 data = np.array(np.genfromtxt("Salary_Prediction/Data/Salary_Data.csv",delimiter=',',skip_header=1))
 X=data[:,0].reshape(-1,1)
@@ -12,3 +13,8 @@ model = linear_model.LinearRegression()
 model.fit(X_train,Y_train)
 Y_pred = model.predict(X_test)
 print('R2 score: %.2f' % r2_score (Y_test,Y_pred))
+
+plt.scatter(X_train, Y_train, color='blue', label="Actual Data")
+plt.scatter(X_test, Y_test, color="black",label="Test Data")
+plt.plot(X_test, Y_pred, color="red", label="Predicted Line")
+plt.show()
